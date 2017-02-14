54e9c69617b83bb764d1ef2bd5e7713f6bf4e943
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-14 22:05:23 +0900

    Add UnicodeScalar extension

---
+++ EastAsianWidth/UnicodeScalar+EastAsianWidth.swift
@@ -0,0 +1,13 @@
+//
+//  UnicodeScalar+EastAsianWidth.swift
+//  EastAsianWidth
+//
+//  Created by Yuki Takahashi on 2017/02/14.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+import Foundation
+
+public extension UnicodeScalar {
+    
+}
