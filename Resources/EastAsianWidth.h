b93a40bc27eb68d97ffee6c035a8a7fe6f290592
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-14 23:21:58 +0900

    Use `Foundation` instead

--- Resources/EastAsianWidth.h
+++ Resources/EastAsianWidth.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017年 waft. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for EastAsianWidth.
 FOUNDATION_EXPORT double EastAsianWidthVersionNumber;
