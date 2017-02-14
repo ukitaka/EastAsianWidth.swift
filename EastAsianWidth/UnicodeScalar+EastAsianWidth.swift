460feb9ace02831b826ab0df3bf913421efb4b6c
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-14 22:37:16 +0900

    Add `isEastAsianWide`

--- EastAsianWidth/UnicodeScalar+EastAsianWidth.swift
+++ EastAsianWidth/UnicodeScalar+EastAsianWidth.swift
@@ -213,7 +213,7 @@ public extension UnicodeScalar {
         }
     }
 
-    /// Neutral (Not East Asian): 
+    /// Neutral (Not East Asian):
     /// All other characters. Neutral characters do not occur in legacy East Asian character sets. By extension, they also do not occur in East Asian typography. For example, there is no traditional Japanese way of typesetting Devanagari. Canonical equivalents of narrow and neutral characters may not themselves be narrow or neutral respectively. For example, U+00C5 Å LATIN CAPITAL LETTER A WITH RING ABOVE is Neutral, but its decomposition starts with a Narrow character.
     /// See: http://unicode.org/reports/tr11/#ED7
     ///      https://github.com/audreyt/Unicode-EastAsianWidth/blob/master/lib/Unicode/EastAsianWidth.pm#L229-L400
@@ -388,6 +388,34 @@ public extension UnicodeScalar {
         case 0xFFF9...0xFFFC: return true
         case 0x10000...0x1D7FF: return true
         case 0xE0001...0xE007F: return true
+        default:
+            return false
+        }
+    }
+
+    /// East Asian Wide (W)
+    /// All other characters that are always wide. These characters occur only in the context of East Asian typography where they are wide characters (such as the Unified Han Ideographs or Squared Katakana Symbols). This category includes characters that have explicit halfwidth counterparts, along with characters that have the UTR51 property Emoji_Presentation, with the exception of the range U+1F1E6 REGIONAL INDICATOR SYMBOL LETTER A through U+1F1FF REGIONAL INDICATOR SYMBOL LETTER Z.
+    /// See: http://unicode.org/reports/tr11/#ED4
+    /// https://github.com/audreyt/Unicode-EastAsianWidth/blob/master/lib/Unicode/EastAsianWidth.pm#L402-L422
+    public var isEastAsianWide: Bool {
+        switch self.value {
+        case 0x1100...0x115F: return true
+        case 0x2329...0x232A: return true
+        case 0x2E80...0x2FFB: return true
+        case 0x3001...0x303E: return true
+        case 0x3041...0x33FF: return true
+        case 0x3400...0x4DB5: return true
+        case 0x4E00...0x9FBB: return true
+        case 0xA000...0xA4C6: return true
+        case 0xAC00...0xD7A3: return true
+        case 0xF900...0xFAD9: return true
+        case 0xFE10...0xFE19: return true
+        case 0xFE30...0xFE6B: return true
+        case 0x20000...0x2A6D6: return true
+        case 0x2A6D7...0x2F7FF: return true
+        case 0x2F800...0x2FA1D: return true
+        case 0x2FA1E...0x2FFFD: return true
+        case 0x30000...0x3FFFD: return true
         default:
             return false
         }
