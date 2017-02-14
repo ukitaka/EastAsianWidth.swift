7b1444dcf2756e2960c814951e32932d68159a73
ukitaka <yuki.takahashi.1126@gmail.com>
2017-02-14 21:51:48 +0900

    Add iOS target

---
+++ Resources/EastAsianWidth.h
@@ -0,0 +1,19 @@
+//
+//  EastAsianWidth.h
+//  EastAsianWidth
+//
+//  Created by Yuki Takahashi on 2017/02/14.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EastAsianWidth.
+FOUNDATION_EXPORT double EastAsianWidthVersionNumber;
+
+//! Project version string for EastAsianWidth.
+FOUNDATION_EXPORT const unsigned char EastAsianWidthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EastAsianWidth/PublicHeader.h>
+
+
