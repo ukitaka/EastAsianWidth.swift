dfd845e63cded3b37f7e0fcddb94f0e2c8b30d43
ukitaka <ukitaka@linecorp.com>
2017-02-15 14:00:23 +0900

    Add `EastAsianNarrowTests`

---
+++ Tests/EastAsianNarrowTests.swift
@@ -0,0 +1,23 @@
+//
+//  EastAsianNarrowTests.swift
+//  EastAsianWidth
+//
+//  Created by Yuki Takahashi on 2017/02/15.
+//  Copyright © 2017年 waft. All rights reserved.
+//
+
+import XCTest
+import EastAsianWidth
+
+class EastAsianNarrowTests: XCTestCase {
+    func testEdgeCases() {
+        EastAsianNarrowEdgeUnicodeScalar.forEach(AssertEastAsianNarrow)
+    }
+
+    func testNonEastAsianNarrowCharacters() {
+        AssertEastAsianNarrow("Hello")
+        AssertNotEastAsianNarrow("こんにちわ")
+        AssertNotEastAsianNarrow("안녕하세요")
+        AssertNotEastAsianNarrow("你好")
+    }
+}
