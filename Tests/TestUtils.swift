c51497948a6aba2d3947283997c6c63cb186768c
ukitaka <ukitaka@linecorp.com>
2017-02-15 13:52:30 +0900

    Add util functions

--- Tests/TestUtils.swift
+++ Tests/TestUtils.swift
@@ -44,3 +44,75 @@ func AssertNotEastAsianNeutral(_ string: String) {
 func AssertNotEastAsianNeutral(_ unicodeScalar: UnicodeScalar) {
     XCTAssertFalse(unicodeScalar.isEastAsianNeutral)
 }
+
+// MARK: - EastAsianHalfwidth
+
+func AssertEastAsianHalfwidth(_ string: String) {
+    string.unicodeScalars.forEach(AssertEastAsianHalfwidth)
+}
+
+func AssertEastAsianHalfwidth(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertTrue(unicodeScalar.isEastAsianHalfwidth)
+}
+
+func AssertNotEastAsianHalfwidth(_ string: String) {
+    string.unicodeScalars.forEach(AssertNotEastAsianHalfwidth)
+}
+
+func AssertNotEastAsianHalfwidth(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertFalse(unicodeScalar.isEastAsianHalfwidth)
+}
+
+// MARK: - EastAsianFullwidth
+
+func AssertEastAsianFullwidth(_ string: String) {
+    string.unicodeScalars.forEach(AssertEastAsianFullwidth)
+}
+
+func AssertEastAsianFullwidth(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertTrue(unicodeScalar.isEastAsianFullwidth)
+}
+
+func AssertNotEastAsianFullwidth(_ string: String) {
+    string.unicodeScalars.forEach(AssertNotEastAsianFullwidth)
+}
+
+func AssertNotEastAsianFullwidth(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertFalse(unicodeScalar.isEastAsianFullwidth)
+}
+
+// MARK: - EastAsianNarrow
+
+func AssertEastAsianNarrow(_ string: String) {
+    string.unicodeScalars.forEach(AssertEastAsianNarrow)
+}
+
+func AssertEastAsianNarrow(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertTrue(unicodeScalar.isEastAsianNarrow)
+}
+
+func AssertNotEastAsianNarrow(_ string: String) {
+    string.unicodeScalars.forEach(AssertNotEastAsianNarrow)
+}
+
+func AssertNotEastAsianNarrow(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertFalse(unicodeScalar.isEastAsianNarrow)
+}
+
+// MARK: - EastAsianWide
+
+func AssertEastAsianWide(_ string: String) {
+    string.unicodeScalars.forEach(AssertEastAsianWide)
+}
+
+func AssertEastAsianWide(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertTrue(unicodeScalar.isEastAsianWide)
+}
+
+func AssertNotEastAsianWide(_ string: String) {
+    string.unicodeScalars.forEach(AssertNotEastAsianWide)
+}
+
+func AssertNotEastAsianWide(_ unicodeScalar: UnicodeScalar) {
+    XCTAssertFalse(unicodeScalar.isEastAsianWide)
+}
